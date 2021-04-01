166bedb979c361eec80c565e98ba3303eb45c1e5
hashtag17th <81721570+hashtag17th@users.noreply.github.com>
2021-04-01 07:39:26 +0100

    Update and rename Install.sh to SLS101.sh
    Alpha comin...SuperLazyScript 1.01...BETA, there are issues on some terminals, given the fact that this uses mate-terminal. There may be an issue on frontend, relating to zsh mate-terminal conflicts/technical changes that MAY cause this script to not work properly on your terminal. 
    This was made in a VirtualBox, Kernel: 5.7.0-2parrot2-amd64 - Shell: zsh 5.8 - DE: MATE 1.24.1 - Terminal: mate-terminal.

--- SLS101.sh
+++ SLS101.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "STAY UPDATED! INSERT PASSWORD TO UPDATE BEFORE LAUNCHING TOOLS!"
-sudo su -c 'mate-terminal --geometry 48x48 -x sudo kalitorify -t' | sudo su -c 'mate-terminal --full-screen --zoom 2.0 /home/usr/bin/cmatrix -x cmatrix' & sudo su -c 'mate-terminal --full-screen --zoom 2.0'
+echo "SuperLazyLaunch..."
+sudo su -c 'mate-terminal --geometry 48x48 -x sudo kalitorify -t' | sudo su -c 'mate-terminal --full-screen /home/usr/bin/cmatrix -x cmatrix' & sudo su -c 'mate-terminal --working-directory /root/lscript set --active --full-screen --zoom 2.0 -x bash l'
 echo "### LAZY IS RUNNING AS ROOT IN ANOTHER WINDOW-MULTITASKING-IS-YOUR-FRIEND;) | HIT ENTER TO SHUTDOWN THE SCRIPT or PRESS CTRL+C TO CLOSE SCRIPT - THIS WILL KEEP KALITORIFY ON ###"
 read
 echo "SuperLazy OSINT-SCRIPT STOPPED! ThanQ for trying me!);)"
 sudo kalitorify -c
 #DESCRIPTION:
 ## OneDollarLiner SuperLazyLaunch with Kalitorify - good & lazy fresh outta the matrix "script" with cherry on top! Made by hashtag17th & scriptkewltweak.
-## Hit small L key (l) when the script is done loading Kalitorify and the root window is highlighted - this will start the Lazy script(if installed) in your new root terminal window while kalitorify and cmatrix is running in the background.
-# Q=ExitsTheCmatrix and you can now hit enter or ctrl+c to shut it down. Or you can un-comment or delete the read command on line 4. IM OUT!
-# TIPS:
-## Adjust your terminal window opacity so that you can see the data in the background.
+## Hit small L key (l) when the script is done loading Kalitorify the Lazy script(if installed) is now opened in your root terminal window while kalitorify and cmatrix is running in the background.
+# Q=ExitsTheCmatrix and you can now hit enter or ctrl+c to shut it down. Or you can un-comment or delete the read command on line 4. ver.SLS1.01.
+# You will be prompted, whether you want to keep Kalitorify running or not, when closing the LazyScript.
+## Adjust your terminal window opacity so that you can see the data in the background. Ver.1.02 will hopefully have fully implemented information-gathering-tools ez2use - and later maybe with UI & executable. Won't touch the opacity functions, that is user preferance.
 ## If Kalitorify is running for the first time it will take priority and will be highlighted OVER the root terminal window.
 ## If Kalitorify is not running for the first time,and it's just renewing your IP, it will be placed behind the root terminal window - with opacity on your terminal window you can verify when IP is given, Kalitorify will autoclose when done.
 ## The intension with this script is to have the root window highlighted with a "kewl" background. And later build more tools into it; f.ex.with PhoneInfoga, Nmap and SparrowWifi+positions=window-placement etc.
